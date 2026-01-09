26b87673dbe8d8b36fa26965c4f8ca8a96f52416
KarChikey <kartikeynegi2002@gmail.com>
2026-01-09 15:19:40 +0530

    first commit

---
+++ String.js
@@ -0,0 +1,21 @@
+// Example 1: Get Length of a String
+// function getLength(str){
+//     console.log("Original String:", str);
+//     console.log("Length of String:", str.length);
+// }
+// getLength("Hello, World!");
+
+// Example 2: Find Index of a Substring
+// function getIndex(str,target){
+//     console.log("Original String:", str);
+//     console.log("Index of String:", str.indexOf(target));
+// }
+// getIndex("hello, world!","world");
+
+// Example 3: find last index of a substring 
+// function getLastIndex(str,target){
+//     console.log("Original String:", str);
+//     console.log("Last Index of String:", str.lastIndexOf(target));
+// }
+// getLastIndex("hello, world! welcome to the world!","world");
+
