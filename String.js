8cc9159d2a4d35be56706c0a772125413ef56083
KarChikey <kartikeynegi2002@gmail.com>
2026-01-09 16:18:57 +0530

    add

--- String.js
+++ String.js
@@ -1,57 +1,57 @@
 // Example 1: Get Length of a String
-// function getLength(str){
-//     console.log("Original String:", str);
-//     console.log("Length of String:", str.length);
-// }
-// getLength("Hello, World!");
+function getLength(str){
+    console.log("Original String:", str);
+    console.log("Length of String:", str.length);
+}
+getLength("Hello, World!");
 
 // Example 2: Find Index of a Substring
-// function getIndex(str,target){
-//     console.log("Original String:", str);
-//     console.log("Index of String:", str.indexOf(target));
-// }
-// getIndex("hello, world!","world");
+function getIndex(str,target){
+    console.log("Original String:", str);
+    console.log("Index of String:", str.indexOf(target));
+}
+getIndex("hello, world!","world");
 
 // Example 3: find last index of a substring 
-// function getLastIndex(str,target){
-//     console.log("Original String:", str);
-//     console.log("Last Index of String:", str.lastIndexOf(target));
-// }
-// getLastIndex("hello, world! welcome to the world!","world");
+function getLastIndex(str,target){
+    console.log("Original String:", str);
+    console.log("Last Index of String:", str.lastIndexOf(target));
+}
+getLastIndex("hello, world! welcome to the world!","world");
 
 // Example 4: Extract a Substring using slice
-// function getSlice(str,start,end){
-//     console.log("Original String:", str);
-//     console.log("Scliced String:", str.slice(start,end));
-// }
-// getSlice("hello, world! welcome to the world!",7,12);
+function getSlice(str,start,end){
+    console.log("Original String:", str);
+    console.log("Scliced String:", str.slice(start,end));
+}
+getSlice("hello, world! welcome to the world!",7,12);
 
 // Example 5: Extract a Substring using substring
-// function getSubstring(str,start,end){
-//     console.log("Original String:", str);
-//     console.log("Substring:", str.substring(start,end));
-// }
-// getSubstring("hello, world! welcome to the world!",7,12);
+function getSubstring(str,start,end){
+    console.log("Original String:", str);
+    console.log("Substring:", str.substring(start,end));
+}
+getSubstring("hello, world! welcome to the world!",7,12);
 
 // Example 6: One more example 
-// const exampleString = "JavaScript is awesome!";
-// let ans1 =exampleString.substr(0,10);
-// let ans2 =exampleString.slice(0,10);
-// console.log(ans1);
-// console.log(ans2);
+const exampleString = "JavaScript is awesome!";
+let ans1 =exampleString.substr(0,10);
+let ans2 =exampleString.slice(0,10);
+console.log(ans1);
+console.log(ans2);
 
 // Example 7: Replace a substring
-// function replaceSubstring(str,oldSub,newSub){
-//     console.log("Original String:", str);
-//     console.log("Replaced String:", str.replace(oldSub,newSub));
-// }
-// replaceSubstring("hello, world!","world","javascript");
+function replaceSubstring(str,oldSub,newSub){
+    console.log("Original String:", str);
+    console.log("Replaced String:", str.replace(oldSub,newSub));
+}
+replaceSubstring("hello, world!","world","javascript");
 
 // Example 8: split a string
-// function splitString(str,delimeter){
-//     console.log("original string:",str);
-//     console.log("split string:",str.split(delimeter));
-// }
-// splitString("hello, world!",",");
+function splitString(str,delimeter){
+    console.log("original string:",str);
+    console.log("split string:",str.split(delimeter));
+}
+splitString("hello, world!",",");
 
 // Example 9: some other inbuilt string methods is trim, toUpperCase, toLowerCase
