81934c3c11a94ddcbcc9b9e3658aa309ed945e51
KarChikey <kartikeynegi2002@gmail.com>
2026-01-10 12:21:03 +0530

    add

---
+++ Array.js
@@ -0,0 +1,28 @@
+//create array 
+// function IinitialArray(arr){
+//     for(let i=0; i<arr.length;i++){
+//         console.log(arr[i]);
+//     }
+// }
+// console.log("Iinitial Array:");
+// IinitialArray([1,2,3,4,5]);
+
+//Example 1: push and unshift method
+// function pushArray(arr,element){
+//     console.log("Original Arrray:",arr);
+//     arr.push(element);
+//     arr.unshift(element);
+//     console.log("After pushing element:",arr);
+// }
+// pushArray([1,2,3,4,5],6)
+
+//Example 2: pop & shift method 
+// function popArray(arr){
+//     console.log("Original Array:",arr);
+//     arr.pop();
+//     arr.shift();
+//     console.log("After poping element:",arr);
+// }
+// popArray([1,2,3,4,5])
+
+//
---
+++ Number.js
@@ -0,0 +1,6 @@
+//Use of parseInt
+// console.log(parseInt("42"));
+// console.log(parseInt("3.14"));
+// console.log(parseInt("78px"));
+
+
