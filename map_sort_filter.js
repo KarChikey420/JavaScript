74539e2ec986bff834633f768287c8eefd3537cb
KarChikey <kartikeynegi2002@gmail.com>
2026-01-11 16:31:44 +0530

    add

--- map_sort_filter.js
+++ map_sort_filter.js
@@ -5,9 +5,16 @@ class Animal{
         this.legs=legs;
         this.voice=voice;
     }
+    speak(){
+        console.log("hey! i am a"+this.name+ "i sound like"+this.voice);
+    }
+    static info(){
+        console.log("there are many types of animals");
+    }
 }
 
+console.log(Animal.info());
 dog=new Animal("dog",4,"bark");
 cat=new Animal("cat",4,"meow");
 
-console.log()
+dog.speak()
