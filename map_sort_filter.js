423ca01825caf64d2bed6b2e49adb0f142c9c15d
KarChikey <kartikeynegi2002@gmail.com>
2026-01-11 16:32:37 +0530

    class method and static method

--- map_sort_filter.js
+++ map_sort_filter.js
@@ -1,20 +1,20 @@
-// map, sort, filter examples
-class Animal{
-    constructor(name,legs,voice){
-        this.name=name;
-        this.legs=legs;
-        this.voice=voice;
-    }
-    speak(){
-        console.log("hey! i am a"+this.name+ "i sound like"+this.voice);
-    }
-    static info(){
-        console.log("there are many types of animals");
-    }
-}
+// class definition and static method
+// class Animal{
+//     constructor(name,legs,voice){
+//         this.name=name;
+//         this.legs=legs;
+//         this.voice=voice;
+//     }
+//     speak(){
+//         console.log("hey! i am a"+this.name+ "i sound like"+this.voice);
+//     }
+//     static info(){
+//         console.log("there are many types of animals");
+//     }
+// }
 
-console.log(Animal.info());
-dog=new Animal("dog",4,"bark");
-cat=new Animal("cat",4,"meow");
+// console.log(Animal.info());
+// dog=new Animal("dog",4,"bark");
+// cat=new Animal("cat",4,"meow");
 
-dog.speak()
+// dog.speak()
