1f98b19e3c491d3d92eb1cd7c4b0d1e263d272d9
KarChikey <kartikeynegi2002@gmail.com>
2026-01-11 17:06:18 +0530

    json methods

--- map_sort_filter.js
+++ map_sort_filter.js
@@ -1,3 +1,4 @@
+
 // class definition and static method
 // class Animal{
 //     constructor(name,legs,voice){
@@ -18,3 +19,28 @@
 // cat=new Animal("cat",4,"meow");
 
 // dog.speak()
+
+// Example 2- Time functions
+
+// const newDate=new Date();
+// console.log("Current date",newDate.getFullYear());
+// console.log("Current month",newDate.getMonth()+1);
+// console.log("Current day",newDate.getDate());
+// console.log("Current hours",newDate.getHours());
+// console.log("Current minutes",newDate.getMinutes());
+// console.log("Current seconds",newDate.getSeconds());    
+
+// Example 3- json methods-
+//            1-json.stringify()
+//            2-json.parse()
+
+const student={
+    name:'kartikey',
+    age:22,
+    course:'MCA'
+}
+const jsonData=JSON.stringify(student);
+console.log("JSON Data:",jsonData);
+
+const objData=JSON.parse(jsonData);
+console.log("Object Data:",objData);
