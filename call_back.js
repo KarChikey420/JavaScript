d9efd6b54bfa860e7ebba290dedcb24bf387068f
KarChikey <kartikeynegi2002@gmail.com>
2026-01-11 14:21:44 +0530

    add

--- call_back.js
+++ call_back.js
@@ -1,9 +1,11 @@
 // Example1: Using a callback function to process an array
-function sum(num1,num2,callback){
-    let sum = num1 + num2;
-    callback(sum);
-}
-function displayResult(data){
-    console.log("The result is:"+data)
-}
-const result=sum(5,10,displayResult);
+// function sum(num1,num2,callback){
+//     let sum = num1 + num2;
+//     callback(sum);
+// }
+// function displayResult(data){
+//     console.log("The result is:"+data)
+// }
+// const result=sum(5,10,displayResult);
+
+// Example 2: 
