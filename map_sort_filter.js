8a1c51e216abb9e3e47a6099e026144da05c0a84
KarChikey <kartikeynegi2002@gmail.com>
2026-01-11 16:24:17 +0530

    add

---
+++ map_sort_filter.js
@@ -0,0 +1,13 @@
+// map, sort, filter examples
+class Animal{
+    constructor(name,legs,voice){
+        this.name=name;
+        this.legs=legs;
+        this.voice=voice;
+    }
+}
+
+dog=new Animal("dog",4,"bark");
+cat=new Animal("cat",4,"meow");
+
+console.log()
