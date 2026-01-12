9e620743e628b70d9b3aaa23e5d54f760325724c
KarChikey <kartikeynegi2002@gmail.com>
2026-01-13 00:13:18 +0530

    add

--- asnyc_fun.js
+++ asnyc_fun.js
@@ -1,16 +1,34 @@
 // basic async function 
 
-function addData(num){
-    let sum=0
-    for (let i=0;i<=num;i++){
-        sum+=i;
-    }
-    return sum;
+// function addData(num){
+//     let sum=0
+//     for (let i=0;i<=num;i++){
+//         sum+=i;
+//     }
+//     return sum;
+// }
+// function process(){
+//     return addData(100);
+// }
+// setTimeout(() => {
+//     console.log(process());
+// }, 1000);
+// console.log("hello world!");
+
+// example 2 : using async and await
+
+function addData(){
+    let p=new Promise(function(resolve){
+        setTimeout(function(){
+            resolve("hii there!");
+        }, 1000)
+    });
+    return p;
 }
-function process(){
-    return addData(100);
+
+async function process() {
+    let val= await addData()
+    console.log(val);
 }
-setTimeout(() => {
-    console.log(process());
-}, 1000);
-console.log("hello world!");
+
+process();
