d3f2de0f77f292582325981117733214f9c1e700
KarChikey <kartikeynegi2002@gmail.com>
2026-01-22 21:47:58 +0530

    add

--- my_first_react_app/backend/index.js
+++ my_first_react_app/backend/index.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import users from './user.js';
 
 const app = express();
 
 app.get('/',(req,res)=>{
     res.send('Hello from Express servers!');
+})
+
+app.get('/api/users',(req,res)=>{
+    res.json(users);
 })
 
 const PORT = process.env.PORT || 5000;
--- my_first_react_app/front_end/user-app/src/App.js
+++ my_first_react_app/front_end/user-app/src/App.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const App = () => {
-  return (
-    <div>
-      Hello World
-    </div>
-  );
+    
+    const [users, setUsers] = React.useState([]);
+
+    const getUsers = () => {
+        fetch("/api/users", {
+            headers: {
+                'Authorization': 'Bearer your-secret-token'
+            }
+        })
+        .then(res => res.json())
+        .then(json => setUsers(json))
+        .catch(err => console.error('Error:', err));
+    }
+
+    useEffect(() => {
+        getUsers();
+    }, [])
+  
+
+    return (
+        <div>
+            {users.map((data) => {
+                return (
+                    <div key={data.id} style={{border:'1px solid gray',margin:'10px',padding:'10px'}}>
+                        <h1>name: {data.name}</h1>
+                        <h1>email: {data.email}</h1>
+                        <h1>username: {data.username}</h1>
+                    </div>
+                );
+            })}
+        </div>
+    );
 };
 
 export default App;
