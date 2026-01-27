defe8442e33bc01c32dfef49ae62b51184365cdf
KarChikey <kartikeynegi2002@gmail.com>
2026-01-27 21:00:21 +0530

    add

--- project-1/backend/index.js
+++ project-1/backend/index.js
@@ -1,4 +1,5 @@
 import  express from "express"
+import { createTodo,upadteTodo} from "./types.js"
 
 const express=require("express");
 const app=express();
@@ -6,8 +7,14 @@ const app=express();
 app.use(express.json());
 
 app.post("/todo",function(req,res){
-
-
+    const createPayload=req.body;
+    const parsePayload=createTodo.safeParse(createPayload);
+    if(!parsePayload.sucess){
+        res.status(400).json({
+            msg:"Invalid Payload",
+        })
+        return;
+    }
 })
 
 app.get("/todos",function(req,res){
@@ -15,5 +22,11 @@ app.get("/todos",function(req,res){
 })
 
 app.put("/completed",function(req,res){
-    
+    const upadtepayload=req.body;
+    const parsePayload=upadteTodo.safeParse(upadtepayload);
+    if(!parsePayload.sucess){
+        res.status(400).json({
+            msg:"Invalid Payload",
+        })
+    }
 })
