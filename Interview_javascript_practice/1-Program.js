7cff4c59b98e9b93541188fddbaeed6f60233d26
KarChikey <kartikeynegi2002@gmail.com>
2026-02-25 13:22:26 +0530

    add

--- Interview_javascript_practice/1-Program.js
+++ Interview_javascript_practice/1-Program.js
@@ -8,15 +8,15 @@ function test(){
 test()
 */
 
-var count=10;
-count=11
-console.log(count)
-function test(){
-    const count=11;
-    count=10
-    console.log(count)
-}
-test()
+// var count=10;
+// count=11
+// console.log(count)
+// function test(){
+//     const count=11;
+//     count=10
+//     console.log(count)
+// }
+// test()
 
 //output 11 because of scope
 
