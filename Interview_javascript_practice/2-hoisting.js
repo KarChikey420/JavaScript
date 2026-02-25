2382aac50f74ec2c62345633c36b4571a9c18b75
KarChikey <kartikeynegi2002@gmail.com>
2026-02-25 13:40:48 +0530

    add

--- Interview_javascript_practice/2-hoisting.js
+++ Interview_javascript_practice/2-hoisting.js
@@ -6,10 +6,12 @@ var count=10;
 
 // let hoisting
 // let throw ref error because it runs program line by line code 
+// it store the count in temp dead zone 
 console.log(count);
 let count=10;
 
-// let hoisting
-// let throw ref error because it runs program line by line code 
+// const hoisting
+//  throw ref error because it runs program line by line code
+// it store the count in temp dead zone  
 console.log(count);
 const count=10;
