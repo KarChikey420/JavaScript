0b89f4200a2e9ad5a388df1624a9a4026a8c2987
KarChikey <kartikeynegi2002@gmail.com>
2026-02-25 17:49:06 +0530

    add

--- Interview_javascript_practice/3-Map_filter_reduce.js
+++ Interview_javascript_practice/3-Map_filter_reduce.js
@@ -20,7 +20,7 @@ const reduceData=nums.reduce((acc,curr,i,nums)=>{ // acc is the previous value a
 
 console.log(reduceData)
 
-//ForEach example -- its change the value in the same array not create other array
+//ForEach example -- its change the value in the same array not create other
 
 const nums=[1,2,3,4,5];
 nums.forEach((num,i) => {
@@ -28,6 +28,20 @@ nums.forEach((num,i) => {
 });
 console.log(nums)
 
+// some practice questions
 
+let students=[
+    {name:'A',rollNumber:1,marks:90},
+    {name:'B',rollNumber:2,marks:80},
+    {name:'C',rollNumber:3,marks:70},
+    {name:'D',rollNumber:4,marks:60}
+];
 
+const names=students.map((student)=>student.name.toUpperCase());
+console.log(names)
 
+const More_than_60=students.filter((student)=>{return student.marks>60})
+console.log(More_than_60)
+
+const MarksSum=students.reduce((acc,curr)=>{return acc+curr.marks},0)
+console.log(MarksSum)
