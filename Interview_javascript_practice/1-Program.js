bad8b2f205509fc429a8d4891b20d07ee7c80849
KarChikey <kartikeynegi2002@gmail.com>
2026-02-25 13:29:43 +0530

    add

--- Interview_javascript_practice/1-Program.js
+++ Interview_javascript_practice/1-Program.js
@@ -1,22 +1,34 @@
 // var, let, const use in javascript
 
-/*var count;
+var count;
 function test(){
     count=10;
     console.log(count)
 }
 test()
-*/
 
-// var count=10;
-// count=11
-// console.log(count)
-// function test(){
-//     const count=11;
-//     count=10
-//     console.log(count)
-// }
-// test()
+// var have function scope
+
+var count=10;
+count=11
+console.log(count)
+function test(){
+    let count=11;
+    count=10
+    console.log(count)
+}
+test()
 
 //output 11 because of scope
+// let has only block scope
 
+
+const num=10;
+function test(){
+    const num=11;
+    console.log(num);
+}
+test()
+
+// output is 11 const does'nt allowed re assignment 
+//const have only block scope 
