7ff4bff1a707d7d89ab7659975018e7db39fa6cd
KarChikey <kartikeynegi2002@gmail.com>
2026-02-25 13:39:32 +0530

    add

---
+++ Interview_javascript_practice/2-hoisting.js
@@ -0,0 +1,15 @@
+// var hoisting
+// var throw undefined 
+console.log(count);
+var count=10;
+
+
+// let hoisting
+// let throw ref error because it runs program line by line code 
+console.log(count);
+let count=10;
+
+// let hoisting
+// let throw ref error because it runs program line by line code 
+console.log(count);
+const count=10;
