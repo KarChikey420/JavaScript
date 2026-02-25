90279cd077eba106d5c0790686d8247ef69da271
KarChikey <kartikeynegi2002@gmail.com>
2026-02-25 13:22:04 +0530

    add

--- Interview_javascript_practice/1-Program.js
+++ Interview_javascript_practice/1-Program.js
@@ -1,2 +1,22 @@
 // var, let, const use in javascript
 
+/*var count;
+function test(){
+    count=10;
+    console.log(count)
+}
+test()
+*/
+
+var count=10;
+count=11
+console.log(count)
+function test(){
+    const count=11;
+    count=10
+    console.log(count)
+}
+test()
+
+//output 11 because of scope
+
