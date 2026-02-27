3c4f009f7262b534b5756fa6e49937ef8100e4aa
KarChikey <kartikeynegi2002@gmail.com>
2026-02-27 19:19:19 +0530

    add

--- Use_Prisma/Raddme.md
+++ Use_Prisma/Raddme.md
@@ -3,3 +3,5 @@
 2-npm install prisma typescript ts-node @types/node --save-dev
 3-npx tsc --init
 4-npx prisma init
+5-npx prisma migrate dev
+6-npx prisma generate
---
+++ Use_Prisma/prisma/migrations/20260227085239_my_data/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("email")
+);
+
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);
