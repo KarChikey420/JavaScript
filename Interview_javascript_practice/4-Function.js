20648772b9010fd8c001464a156662c1250eb585
KarChikey <kartikeynegi2002@gmail.com>
2026-03-01 21:58:17 +0530

    add

--- Interview_javascript_practice/4-Function.js
+++ Interview_javascript_practice/4-Function.js
@@ -1,23 +1,22 @@
 //callback function
-// function square(num){
-//     return num*num;
-// }
+function square(num){
+    return num*num;
+}
 
-// function printSquare(fn){
-//     const result=fn(5);
-//     console.log(result);
-// }
+function printSquare(fn){
+    const result=fn(5);
+    console.log(result);
+}
 
-// printSquare(square);
+printSquare(square);
 
 // IFEE (Immediately Invoked Function Expression)
 
-// (function add(num1,num2){
-//     console.log(num1+num2);
-// }(5,10));
+(function add(num1,num2){
+    console.log(num1+num2);
+}(5,10));
 
 //Arrow function
-
 const MyName=(name)=>{
     console.log("my name is"+""+name);
 }
