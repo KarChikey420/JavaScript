69fe734c6b3219502f146fa990940b30ca051e57
KarChikey <kartikeynegi2002@gmail.com>
2026-03-01 21:51:03 +0530

    add

--- Interview_javascript_practice/4-Function.js
+++ Interview_javascript_practice/4-Function.js
@@ -12,6 +12,6 @@
 
 // IFEE (Immediately Invoked Function Expression)
 
-(function add(num1,num2){
-    console.log(num1+num2);
-}(5,10));
+// (function add(num1,num2){
+//     console.log(num1+num2);
+// }(5,10));
