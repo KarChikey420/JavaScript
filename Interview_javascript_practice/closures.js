4f563272a9ae24cc80062eaa48cf8b37230430f9
KarChikey <kartikeynegi2002@gmail.com>
2026-03-02 22:14:45 +0530

    add

--- Interview_javascript_practice/closures.js
+++ Interview_javascript_practice/closures.js
@@ -16,3 +16,16 @@ function Outer(){
     return Inner;
 }
 Outer()(5)
+
+ // closure chaining- if inner function is able to access outer variable of its parent function and so on
+ var e=10;
+ function sum(a){
+    return function(b){
+        return function(c){
+            return function(d){
+                return a+b+c+d+e;
+            };
+        };
+    };
+ }
+ console.log(sum(1)(2)(3)(4));
