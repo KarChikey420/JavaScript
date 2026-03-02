2233794cefe664bbdcf90cbf34c1d1df22da8006
KarChikey <kartikeynegi2002@gmail.com>
2026-03-02 22:09:39 +0530

    add

--- Interview_javascript_practice/closures.js
+++ Interview_javascript_practice/closures.js
@@ -5,3 +5,14 @@ function Local(){
     console.log(name);
 }
 Local();
+
+//closure- if inner function is able to access outervariable even after the outer function has executed
+
+function Outer(){
+    var name = "Kartikey";
+    function Inner(num){
+        console.log(name, num);
+    }
+    return Inner;
+}
+Outer()(5)
