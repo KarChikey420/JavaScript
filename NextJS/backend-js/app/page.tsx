801e1d2ef877a91ecd8a0452006d2d646ef85afa
KarChikey <kartikeynegi2002@gmail.com>
2026-04-03 23:33:52 +0530

    use with rect componant

--- NextJS/backend-js/app/page.tsx
+++ NextJS/backend-js/app/page.tsx
@@ -1,9 +1,19 @@
 import Image from "next/image";
+import axios from "axios";
+import { asyncWrapProviders } from "async_hooks";
 
-export default function Home() {
-  return (
+async function getData(){
+  const response=await axios.get("http://192.168.56.1:3000/api/user")
+  return response.data;
+}
+
+export default async function Home(){
+  const data=await getData();
+  return(
     <div>
-      <h1>hii there!</h1>
+      <h1>{data.name}</h1>
+      <p>{data.email}</p>
     </div>
-  );
+  )
+  
 }
