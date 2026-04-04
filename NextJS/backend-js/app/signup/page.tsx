3b1e0b8e334736ced5540036ad24b60a96bef697
KarChikey <kartikeynegi2002@gmail.com>
2026-04-04 23:36:58 +0530

    add

---
+++ NextJS/backend-js/app/signup/page.tsx
@@ -0,0 +1,7 @@
+export function Signup(){
+    return <div>
+        <input type="text" placeholder="Username"/>
+        <input type="password" placeholder="Password"/>
+        <button>Signup</button>
+    </div>
+}
