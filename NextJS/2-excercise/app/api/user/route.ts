0ecc12475564da9bf24f825f48ab39e3999e3e98
KarChikey <kartikeynegi2002@gmail.com>
2026-04-05 15:33:25 +0530

    ready for backend

--- NextJS/2-excercise/app/api/user/route.ts
+++ NextJS/2-excercise/app/api/user/route.ts
@@ -1,5 +1,12 @@
 import { NextRequest } from "next/server";
 
-export function POST(req: NextRequest){
-    
+export async function POST(req: NextRequest) {
+    const body = await req.json();
+
+    console.log("Received user data:", body);
+
+    return Response.json({
+        message:"User data received successfully"
+    })
+
 }
