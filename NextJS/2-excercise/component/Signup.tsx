4aa406ba9d8ee20e71ce2c38323f5195e51347f0
KarChikey <kartikeynegi2002@gmail.com>
2026-04-05 15:04:56 +0530

    SignUp page for

---
+++ NextJS/2-excercise/component/Signup.tsx
@@ -0,0 +1,9 @@
+import { use, useState } from "react"
+
+export function Signup(){
+    const [username, setusername]=useState("");
+    const[password, setpassword]=useState("");
+    return <div>
+
+    </div>
+}
