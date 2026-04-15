14eb80068466544d7c7c12becb49bce4305c2a56
KarChikey <kartikeynegi2002@gmail.com>
2026-04-15 16:57:01 +0530

    loops

---
+++ Interview_javascript_practice/loops.js
@@ -0,0 +1,22 @@
+// in javscript we have 5 types of loops
+// 1. for loop
+
+for (let i =0; i<5;i++){
+    console.log(i);
+}
+
+//2. while loop
+
+let i=0;
+while(i<5){
+    console.log(i);
+    i++;
+}
+
+//3. do while loop
+
+let j=0;
+do{
+    console.log(j);
+    j++;
+}while(j<5)
