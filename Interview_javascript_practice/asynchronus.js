c458c17ad771620f81341f423a42c9daaba96719
KarChikey <kartikeynegi2002@gmail.com>
2026-04-17 23:09:22 +0530

    promises

--- Interview_javascript_practice/asynchronus.js
+++ Interview_javascript_practice/asynchronus.js
@@ -11,3 +11,16 @@ console.log("after settimeout");
 //3. fetching data from an API
 //4. promises
 //5. animations 
+
+// promises 
+// it is use for handling asynchornous operation in javascript 
+
+const myPromise=new Promise((resolve,reject)=>{
+    const success=true;
+    if (success){
+        resolve("promise resolved sucessfully");
+    }else{
+        reject("promise rejected");
+    }
+})
+ console.log(myPromise);
